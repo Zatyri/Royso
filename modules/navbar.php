e96bb6a3bca6bd10cea6cf041586a9ad119a8d54
Oskar <gustosk@gmail.com>
2020-12-11 20:42:44 +0200

    initial commit

---
+++ modules/navbar.php
@@ -0,0 +1,8 @@
+<nav>
+  <div class='container'>
+    <button type="button" class="btn btn-outline-secondary">Hem</button>
+    <button type="button" class="btn btn-outline-secondary">Test2</button>
+    <button type="button" class="btn btn-outline-secondary">Test3</button>
+
+  </div>
+</nav>
